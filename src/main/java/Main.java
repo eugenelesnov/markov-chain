3f7ab8222bb3600fc928f087fa9bad465da728c2
EugeneLesnov <zheka.lesnov1996@gmail.com>
2019-08-25 00:33:13 +0300

    added number of words per state as parameter

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,31 +4,31 @@
 
 public class Main {
 
-    private static final int WORDS_PER_STATE = 3;
-
     public static void main(String[] args) {
         String pathToFile = "";
         int sentenceLength = 0;
+        int wordsPerState = 0;
 
         try {
             pathToFile = args[0];
             sentenceLength = Integer.parseInt(args[1]);
+            wordsPerState = Integer.parseInt(args[2]);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
 
-        if (!pathToFile.isEmpty() && sentenceLength > 0) {
+        if (!pathToFile.isEmpty() && sentenceLength > 0 && wordsPerState > 0) {
             Text text = new Text();
             text.initText(pathToFile);
 
             String[] normalizedText = text.getWords();
 
-            MarkovChain markovChain = new MarkovChain(normalizedText, WORDS_PER_STATE);
+            MarkovChain markovChain = new MarkovChain(normalizedText, wordsPerState);
             String result = markovChain.compose(sentenceLength);
             text.writeToFile(result);
         } else {
             throw new IllegalArgumentException("Wrong arguments: pathToFile = " + pathToFile
-                    + " sentenceLength: " + sentenceLength);
+                    + ", sentenceLength: " + sentenceLength + ", wordsPerState = " + wordsPerState);
         }
     }
 }
